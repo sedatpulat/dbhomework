240887cda5b002abe4909539a4609516bda89565
Sedat <sedatpulat_32@hotmail.com>
2023-12-24 21:47:19 +0300

    Add files via upload

---
+++ DatabaseHomeWork.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DatabaseHomeWork.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
